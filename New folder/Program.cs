ac9d3f05e1b5bc272268c0e0fa3f391ff3b8cc96
AbhishekYevle <105035736+AbhishekYevle@users.noreply.github.com>
2023-01-25 17:14:44 +0530

    Add files via upload

---
+++ New folder/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace try_generic
+{
+  class program
+  {
+    private static void Main(string[] args)
+    {
+      charachters();
+      numbers();
+    }
+    public static void charachters()
+    {
+      bool equal = calculator<string>.IsEqual("B", "M");
+
+      if (equal)
+      {
+        Console.WriteLine("Equal");
+      }
+      else
+      {
+        Console.WriteLine("Not Equal");
+      }
+
+    }
+    public static void numbers()
+    {
+      bool equal = calculator<int>.IsEqual(10, 10);
+      if (equal)
+      {
+        Console.WriteLine("Equal");
+      }
+      else
+      {
+        Console.WriteLine("Not Equal");
+      }
+
+    }
+  }
+  class calculator<T>
+  {
+    public static bool IsEqual(T value1, T value2)
+    {
+      return value1.Equals(value2);
+    }
+  }
+}
