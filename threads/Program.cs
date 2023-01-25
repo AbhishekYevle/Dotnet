9e5c967333881de667a47580c72d6f9f549f3e94
AbhishekYevle <105035736+AbhishekYevle@users.noreply.github.com>
2023-01-25 17:21:50 +0530

    Add files via upload

---
+++ threads/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading;
+
+namespace threads
+{
+  class ThreadTest
+  {
+    static void Main()
+    {
+      Thread t = new Thread(new ThreadStart(First));
+
+      t.Start();   // Run Go() on the new thread.
+      Go();        // Simultaneously run Go() in the main thread.
+    }
+    static void First()
+    {
+      Console.WriteLine("Hello1");
+    }
+    static void Go()
+    {
+      Console.WriteLine("hello!");
+    }
+  }
+}
---
+++ trial/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace program
+{
+  class pro1
+  {
+    public static void Main(string[] args)
+    {
+      Console.WriteLine("hello I'm Bot ");
+      Console.Write("hello");
+      Console.Write("I'm Abhishek");
+      String myName = Console.ReadLine();
+
+      Console.WriteLine(myName);
+
+    }
+  }
+}
